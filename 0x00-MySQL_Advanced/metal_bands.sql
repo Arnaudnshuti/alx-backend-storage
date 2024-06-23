f18d5675795cceacc09582739c804f7bc6ad547c
Arnaudnshuti <arnaudnshutidjadja@gmail.com>
2024-06-23 10:19:01 -0700

    3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/metal_bands.sql
@@ -0,0 +1,12 @@
+Enter password: 
+bob@dylan:~$ 
+bob@dylan:~$ cat 3-glam_rock.sql | mysql -uroot -p holberton 
+Enter password: 
+band_name   lifespan
+Alice Cooper    56
+Mötley Crüe   34
+Marilyn Manson  31
+The 69 Eyes 30
+Hardcore Superstar  23
+Nasty Idols 0
+Hanoi Rocks 0
